47b6a16a9f8fcffe0e358e3050413cead952ca06
AramaJava <abr-aram@yandex.ru>
2023-03-09 21:49:38 +0500

    ddd1

---
+++ src/main/java/org/example/config/ApplicationInitializer.java
@@ -0,0 +1,22 @@
+package org.example.config;
+
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+}
---
+++ src/main/java/org/example/service/CatService.java
@@ -0,0 +1,24 @@
+package org.example.service;
+
+import org.example.model.Cat;
+import org.example.repository.CatRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class CatService {
+    @Autowired
+    private CatRepository repo;
+
+    public void start() {
+        System.out.println(repo.findAll());
+    }
+
+    public List<Cat> getAllCats() {
+        return repo.findAll();
+    }
+
+    public void createCat (Cat cat) {
+        repo.create(cat);
+    }
+}
